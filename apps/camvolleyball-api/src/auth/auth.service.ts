acd5c368df990d0175909832abaab2410bd22c7a
veyo <veyo.nuon@cimb.com>
2025-12-14 14:06:57 +0700

    Adapt running server to mobile in development state

--- apps/camvolleyball-api/src/auth/auth.service.ts
+++ apps/camvolleyball-api/src/auth/auth.service.ts
@@ -83,7 +83,7 @@ export class AuthService {
 
         const payload = { sub: user.id, phone: user.phoneNumber, roles: user.roles.map(r => r.name) };
         return {
-            access_token: this.jwtService.sign(payload),
+            accessToken: this.jwtService.sign(payload),
             user: {
                 id: user.id,
                 phoneNumber: user.phoneNumber,
--- apps/camvolleyball-api/src/main.ts
+++ apps/camvolleyball-api/src/main.ts
@@ -8,6 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('camvolleyball/v1');
   app.useGlobalInterceptors(new TransformInterceptor());
@@ -29,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
